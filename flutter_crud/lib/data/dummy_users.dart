e063586f0481fb2d328da915922f6d19b2dda318
EmersonSperb <emersonsperb1983@gmail.com>
2020-10-04 16:12:56 -0300

    Cadastro

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Paula Tejando',
+      email: 'paula@dentro.teu.cu',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Jalim Rabei',
+      email: 'ja@lim.ra.bei',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Simas Turbo',
+      email: 'simas@turbo.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png'),
+};
